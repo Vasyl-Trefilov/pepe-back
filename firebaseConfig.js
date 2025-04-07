66f36111f996c468bf25663a4c5a30834673e39c
Vasyl-Trefilov <drenfor228@gmail.com>
2025-04-07 22:36:30 +0200

    db import fix2

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 const admin = require("firebase-admin");
+console.log(
+  "Loaded private key starts with:",
+  process.env.FIREBASE_PRIVATE_KEY.slice(0, 30)
+);
+console.log("Ends with:", process.env.FIREBASE_PRIVATE_KEY.slice(-30));
 
 const serviceAccount = {
   type: "service_account", // не нужен в .env
@@ -14,11 +19,26 @@ const serviceAccount = {
   client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
   universe_domain: "googleapis.com",
 };
+try {
+  const parsedKey = JSON.parse(`"${process.env.FIREBASE_PRIVATE_KEY}"`);
+  console.log("✅ Ключ парсится нормально");
+} catch (err) {
+  console.error("❌ Проблема с парсингом ключа:", err);
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 console.log("Firebase Admin SDK инициализировано!");
+admin
+  .auth()
+  .listUsers(1)
+  .then((res) => {
+    console.log("✅ Firebase авторизация работает");
+  })
+  .catch((err) => {
+    console.error("❌ Ошибка авторизации Firebase:", err.message);
+  });
 
 const db = admin.firestore();
 module.exports = db;
