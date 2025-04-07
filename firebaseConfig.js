6fa4c686f9e4365cae0cf239e12e378ad6d07e0d
Vasyl-Trefilov <drenfor228@gmail.com>
2025-04-07 13:56:22 +0200

    adding admin2

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./pepeplush-firebase-adminsdk-fbsvc-c3aeeb519c.json");
+const serviceAccount = process.env.ADMIN_DB;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
